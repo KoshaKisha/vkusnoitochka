8ea761f780763c587d0f46f1d66f192717b22e8d
Ekaterina Zhabrova <e.zhabrova@3l.ru>
2025-06-15 22:19:29 +0300

    add desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'vkusno i tochka',
-  description: "Удобное приложение для сотрудников разных должностей",
+  title: "Вкусно и точка — приложение для сотрудников",
+  description: "Удобное и функциональное приложение для управления сменами, отпусками и задачами сотрудников всех уровней.",
   icons: {
-    icon: "/burger.png",
+    icon: "/burger.png", // favicon
   },
 }
 
